5b16ea5415d1a3b7040510026dd22606c40e3935
AlthafHussainK <althafakm@gmail.com>
2021-03-02 12:09:19 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* Open Modal */
+
+document.getElementById('open-modal').addEventListener('click', function(){
+  document.getElementById('overlay').style.display = 'block'
+})
+
+/* close modal */
+
+document.getElementById('close-modal').addEventListener('click', function(){
+  document.getElementById('overlay').style.display = 'none'
+})
